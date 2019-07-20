653e3c26ae3fe2472d5169ced8adc5061001c0bc
emcas88 <emcas88@gmail.com>
2019-07-20 18:43:58 -0400

    Initial ui scaffolding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="add-feed-container">
+        <form className="flex-container add-feed-form">
+          <input className="add-feed-input" type="text" name="Feed url" placeholder="feed url"/>
+          <input className="add-feed-submit" type="submit" value="Add feed"></input>
+        </form>
+      </div>
+
+      <div className="flex-container feed-cards-container">
+        <div className="feed-card-element">
+          
+          <div className="feed-title">
+            <h2>This is the title</h2>
+          </div>
+
+          <div className="feed-headlines">
+            <ul>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div className="feed-card-element">
+          
+          <div className="feed-title">
+            <h2>This is the title</h2>
+          </div>
+
+          <div className="feed-headlines">
+            <ul>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div className="feed-card-element">
+          
+          <div className="feed-title">
+            <h2>This is the title</h2>
+          </div>
+
+          <div className="feed-headlines">
+            <ul>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+              <li><a>Coffee</a></li>
+            </ul>
+          </div>
+
+        </div>
+
+        
+      </div>
     </div>
   );
 }
